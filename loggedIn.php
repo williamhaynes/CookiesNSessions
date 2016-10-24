713718356530ab9a9be340eb37993b6f6a82ed65
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-24 14:25:53 +0100

    updated php code due to bracketing mistakes 2

--- loggedIn.php
+++ loggedIn.php
@@ -12,11 +12,11 @@ displayAccessLevelInformation($accesslevel);
 
 function displayAccessLevelInformation($accessLevel){
     if($accessLevel=="standarduser"){
-        echo "<p> style = \"background-color: lightgreen\">You are currently logged in as a Standard User</p>";
+        echo "<p style = \"background-color: lightgreen\">You are currently logged in as a Standard User</p>";
     }
     elseif($accessLevel =="root"){
-        echo "<p> style = \"background-color: red\">You are currently logged in as a Root User</p>";
-        echo "<p> style = \"background-color: red\">You now have additional admin features</p>";
+        echo "<p style = \"background-color: red\">You are currently logged in as a Root User</p>";
+        echo "<p style = \"background-color: red\">You now have additional admin features</p>";
     }
 }
 
